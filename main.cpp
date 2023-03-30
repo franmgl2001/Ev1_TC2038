a027386db9a17bb7ae8487bbc5271d7fa829b6bc
Francisco Martinez Gallardo <franmgl@Franciscos-MacBook-Pro.local>
2023-03-29 22:24:19 -0600

    Add load functionality

--- main.cpp
+++ main.cpp
@@ -5,20 +5,31 @@
 using namespace std;
 
 // Function: prototypes
-string read_file(string file_name);
+string load_file(string file_name);
 
 
 int main()
 {
+    // Load the transmission file
+    string file = load_file("transmission.txt");
+    string mcode1 =load_file("mcode1.txt");
+    string mcode2 =load_file("mcode2.txt");
+    string mcode3 =load_file("mcode3.txt");
+
 
 }
 
-string read_file(string file_name)
+string load_file(string file_name)
 {
-    ifstream file;
-    file.open(file_name);
     string line;
-    string text;
-    while (getline(file, line))
+    ifstream myfile (file_name);
+    if (myfile.is_open())
     {
-        text += line + "
+        getline (myfile,line);
+        myfile.close();
+    }
+
+    else cout << "Unable to open file";
+
+    return line;
+}
